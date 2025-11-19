0458ac75fb37099e08b85e4ebc856b9269c50e2c
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-20 01:56:43 +0600

    Find the Transpose.c

--- Find the Transpose.c
+++ Find the Transpose.c
@@ -1,1 +1,47 @@
 
+#include <stdio.h>
+
+int main() {
+    int a[10][10], transpose[10][10];
+    int row, col;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &row);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &col);
+
+  
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+  
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+  
+    printf("\nOriginal Matrix:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+ 
+    printf("\nTranspose Matrix:\n");
+    for (int i = 0; j < row; j++) {
+        for (int j = 0; j < col; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
