bd83dc690e539890d7aa3456221a8b1ca053aece
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-20 02:10:33 +0600

    Add two matrices.c

--- Add two matrices.c
+++ Add two matrices.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], sum[10][10];
+    int row, col;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &row);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &col);
+
+  
+    printf("\nEnter elements of first matrix:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+  
+
+   
+    printf("\nEnter elements of second matrix:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+   
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+ 
+    printf("\nResultant Matrix after Addition:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
